da8a21085d18f67a39589a6ae098a2dd5f9166cf
Waleed Gadelkareem <gadelkareem@gmail.com>
2019-10-13 11:52:09 +0200

    Update anticaptcha.go

--- anticaptcha.go
+++ anticaptcha.go
@@ -49,8 +49,8 @@ func (c *Client) createTaskRecaptcha(websiteURL string, recaptchaKey string) (fl
 	responseBody := make(map[string]interface{})
 	json.NewDecoder(resp.Body).Decode(&responseBody)
 	// TODO treat api errors and handle them properly
-	if responseBody["taskId"] == nil {
-		return 0, errors.Errorf("Failed to get a response")
+	if reflect.ValueOf(responseBody["taskId"]).IsNil() {
+		return "", errors.Errorf("Failed to get a response")
 	}
 	return responseBody["taskId"].(float64), nil
 }
